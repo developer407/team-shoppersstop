88e9e8c3167ed34c4aae1854545d67db94dc1257
kosachin <80893464+kosachin@users.noreply.github.com>
2021-12-19 00:38:35 +0530

    updated addToBag page

--- cart/addToBag.js
+++ cart/addToBag.js
@@ -8,3 +8,48 @@ continueBtn.onclick = ()=>{
 
 const products = JSON.parse(localStorage.getItem("cardProducts"));
 
+const itemLen  = document.querySelector("#h1");
+itemLen.innerHTML =`(${products.length} Items)`;
+
+let total = 0;
+products.map((a) => {
+    total += parseInt(a.offPrice);
+}, 0)
+
+const leftDiv = document.querySelector(".leftContentDiv");
+window.onload = () => {
+    products.forEach((product) => {
+        let totalBox = document.querySelector(".cart-head>span>h2");
+        totalBox.innerHTML = `RS ${total}`;
+        let leftBoxContent = `
+        <div class="product-slide-box">
+        <div class="img-left-div">
+            <img width="100%" height="100%"  src="${product.imgUrl}" alt="">
+
+          <div class="img-right-div">
+            <p>${product.brand}</p>
+            <h5>${product.description}</h5>
+            <div>
+              <h3>Color:<span>No Color</span></h3>
+            </div>
+            <div>
+              <h3>Size:<span>${product.size}</span></h3>
+            </div>
+          </div>
+          <div class = "delivery-availibility">
+            <p>Typically delivered in 3-5 working days</p>
+            <p>Cash On Delivery</p>
+            <p>Express Store Pick Up</p>
+            <div class="product-price">
+              <p>Rs ${product.price}</p>
+              <p>Rs ${product.offPrice}</p>
+            </div>
+          </div>
+          </div>
+          </div>
+        `;
+        let div = document.createElement('div');
+        div.innerHTML = leftBoxContent;
+        leftDiv.insertAdjacentElement("beforeend", div);
+    })
+}
